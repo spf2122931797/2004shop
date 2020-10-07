8401f9e795637f1f5f59a62caa2fba9068d768c8
sunpengfei <2122931797@qq.com>
2020-10-07 15:33:35 +0800

    初始化

---
+++ app/Http/Model/Admin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    protected $table = "";
+    protected $primaryKey = "";
+    public $timestamps = false;
+    protected $guarded = [];
+}
